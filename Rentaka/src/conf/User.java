a2630e23d4ebb6c3c67dfe367dcfe269f188db20
ashima15448 <32170935+ashima15448@users.noreply.github.com>
2018-09-11 15:06:32 +0530

    Update User.java

--- Rentaka/src/conf/User.java
+++ Rentaka/src/conf/User.java
@@ -5,10 +5,101 @@
  */
 package com.online_car_rental_syatem.dao;
 
+import java.util.*;
+
 /**
  *
  * @author ashim
  */
 public class User {
+    private int user_id,contact;
+    private String first_name,last_name,dl_number,address,email,status,password;
+    private Date dob;
+    private long aadhar_number;
+
+    public User() {
+    }
+
     
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getDl_number() {
+        return dl_number;
+    }
+
+    public void setDl_number(String dl_number) {
+        this.dl_number = dl_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public long getAadhar_number() {
+        return aadhar_number;
+    }
+
+    public void setAadhar_number(long aadhar_number) {
+        this.aadhar_number = aadhar_number;
+    }
+
+
 }
