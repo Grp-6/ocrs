a7b6d61c7763e27a0863865a4afe38c5fc42bd97
ashima15448 <32170935+ashima15448@users.noreply.github.com>
2018-09-11 15:08:05 +0530

    Update DatabaseConnection.java

--- Rentaka/src/conf/DatabaseConnection.java
+++ Rentaka/src/conf/DatabaseConnection.java
@@ -5,10 +5,41 @@
  */
 package com.online_car_rental_syatem.dao;
 
-/**
- *
- * @author ashim
- */
-public class DatabaseConnection {
-    
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class DatabaseConnection
+{
+    private Connection conn;
+    private Statement stmt;
+    private ResultSet res;
+    public Connection setConnection()
+    {
+        try
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver]");
+            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/online_car_rental_system", "ashima", "ashima");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    public ResultSet getResult(String sql, Connection conn)
+    {
+        this.conn=conn;
+        try
+        {
+            stmt = conn.createStatement();
+            res = stmt.executeQuery(sql);
+        }
+        catch (Exception e)
+        {
+            System.out.println("I found error here");
+        }
+        return res;
+    }
 }
